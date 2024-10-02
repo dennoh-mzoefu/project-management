6304631871e0ceb1e672efdcad5352f8225d52e8
dennoh-mzoefu <denisgichana162@gmail.com>
2024-10-02 18:00:00 +0300

    search backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import projectRoutes from "./routes/projectRoutes";
 import taskRoutes from "./routes/taskRoutes";
+import searchRoutes from "./routes/searchRoutes";
 
 /* ROUTE IMPORTS */
 
@@ -28,7 +29,7 @@ app.get("/", (req, res) => {
 
 app.use("/projects", projectRoutes);
 app.use("/tasks", taskRoutes);
-
+app.use("/search", searchRoutes);
 
 /* SERVER */
 const port = Number(process.env.PORT) || 3000;
